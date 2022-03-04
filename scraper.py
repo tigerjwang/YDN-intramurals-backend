bd2c881528c60097045aeba79c35956377f04faf
Tiger J. Wang <tiger.wangj@gmail.com>
2022-03-04 15:51:58 -0500

    cleanup

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import csv
+import sys
 import os.path
 
 from google.auth.transport.requests import Request
@@ -9,24 +10,10 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-API_KEY = 'AIzaSyDsmqiCdJ2z36NFseEizttPecJHraQaLAE'
-
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-
-# The ID and range of a sample spreadsheet.
 STANDINGS_SPREADSHEET_ID = '1xffq-cDUbEzLkenPLQDPCz9U9qfUWMqPBDUUHg-Ak0Y'
-SAMPLE_RANGE_NAME = 'overall!J2:J15'
 
-# class Sheet(Enum):
-#     OVERALL
-#     WHOOPS
-#     MHOOPS
-#     BHOOPS
-#     CHOOPS
-#     VOLLEYBALL
-#     DODGEBALL
-
+DEBUG = True
 
 def scrape_standings(sheet, sheet_name):
     college_column = {
@@ -47,35 +34,43 @@ def scrape_standings(sheet, sheet_name):
         "Volleyball": "J",
         "Dodgeball": "J"
     }
-    college_range = f'{sheet_name}!{college_column[sheet_name]}2:{college_column[sheet_name]}15'
-    score_range = f'{sheet_name}!{score_column[sheet_name]}2:{score_column[sheet_name]}15'
+    row_start = 2
+    row_end = 15
+    college_range = f'{sheet_name}!{college_column[sheet_name]}{row_start}:{college_column[sheet_name]}{row_end}'
+    score_range = f'{sheet_name}!{score_column[sheet_name]}{row_start}:{score_column[sheet_name]}{row_end}'
+
+    # Invoke Google Sheets API
     try:
         college_result = sheet.values().get(spreadsheetId=STANDINGS_SPREADSHEET_ID,
                                             range=college_range).execute().get('values', [])
         score_result = sheet.values().get(spreadsheetId=STANDINGS_SPREADSHEET_ID,
                                             range=score_range).execute().get('values', [])
-        if college_result and score_result:
-            print(college_result)
-            print(score_result)
-            # with open('names.csv', 'w', newline='') as csvfile:
-            #     fieldnames = ['first_name', 'last_name']
-            #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-            #     writer.writeheader()
-            #     writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-            #     writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-            #     writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
-        else:
-            print('No data found on college or score column.')
     except HttpError as err:
         print(err)
+    
+    # Output results to CSV
+    if college_result and score_result:
+        if DEBUG: print("college_result:", college_result)
+        if DEBUG: print("score_result:", score_result)
+        with open(f'standings_{sheet_name.lower()}.csv', 'w', newline='') as csvfile:
+            fieldnames = ['college', 'score']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for i in range(row_end - row_start + 1):
+                writer.writerow({'college': college_result[i][0], 'score': score_result[i][0]})
+    else:
+        print('No data found on college or score column.')
     
     
 
 def main():
-    service = build('sheets', 'v4', developerKey=API_KEY)
+    if (len(sys.argv) != 2):
+        print("Usage: ./scraper API_KEY")
+        return
 
-    # Call the Sheets API
+    api_key = sys.argv[1]
+    service = build('sheets', 'v4', developerKey=api_key)
+
     sheet = service.spreadsheets()
 
     scrape_standings(sheet, "WHoops")
